e9c9ecd83f91162d5bd0e0e41524102c46adbbce
Dim3ji <fataibada@gmail.com>
2023-11-22 03:51:22 +0100

    Created README and added source files

---
+++ README.md
@@ -0,0 +1,21 @@
+## Overview
+This script checks and scores a given password for comformity with standard and some extended password policies; runs it against a custom password list consisting of common, default and leaked passwords for possible matches.
+
+## Dependencies
+The following python modules are required for this script to run:
+
+* Rich module: If not installed, run `pip install rich` or `python -m pip install rich`
+* Emoji module: if not installed, run `pip install emoji` or `python -m pip install emoji`
+* Colorama module: If not installed, run `pip install colorama` or `python -m pip install colorama`
+
+## How to use
+**Step 1:** Clone repository
+
+**Step 2:** Install dependencies
+
+**Step 3:** Simply run `python password_checker.py` or `python3 password_checker.py` depending on your terminal configuration.
+
+
+**This tool is targeted at the following:**
+* System Admins: For user accounts audits
+* Cybersecurity enthusiasts: For online accounts security foolproof
---
+++ password_checker.py
@@ -0,0 +1,43 @@
+# A password complexity checking tool that also checks given password against common passwords
+import re
+import emoji
+from colorama import Fore, Style
+from rich.progress import track
+
+special = re.compile("[!#$%&'()*+,-./:;<=>?@[\]^_`{|}~]")
+complexity = 0
+
+user_password = input("\nEnter a desired password: ")
+
+#Defining scoring criteria
+if len(user_password) < 8:
+    complexity -= 1
+if len(user_password) > 12:
+    complexity += 1
+if len(user_password) > 18:
+        complexity += 1
+if re.search(special, user_password):
+        complexity += 2
+if re.search("[A-Z]", user_password):
+    complexity += 1
+if re.search("[0-9]", user_password):
+    complexity += 2
+
+#Using colors to indicate bad or good complexity score
+if complexity <= 3:
+    print(f"\nYour password complexity score is {Fore.RED}{complexity}{Fore.RESET}/7 {emoji.emojize(':thumbs_down:')}")
+elif complexity > 3:
+     print(f"\nYour password complexity score is {Fore.GREEN}{complexity}{Fore.RESET}/7 {emoji.emojize(':thumbs_up:')}")
+
+
+#Testing password security against known common passwords
+with open("combined-pass-list.txt","r", encoding="ISO 8859-1") as file:
+    common_pass = file.read().split()
+
+print(f"\n-----Checking your password {Style.BRIGHT}{user_password}{Style.RESET_ALL} against common {Style.BRIGHT}{len(common_pass)}{Style.RESET_ALL} passwords-----\n")
+
+if user_password in track(common_pass, description='[green]Please wait...'):
+    print(f"\nFound in common passwords list, can be cracked in less than a min {emoji.emojize(':crying_face:')}; not secure!")
+else:
+    print(f"\nPassword is secure, enough to waste the hackers time a lil! {emoji.emojize(':winking_face:')}")
+
