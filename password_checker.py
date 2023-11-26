cef69c2b0ae2e640ee48fdccd984934027198ceb
sixth-sensei <fataibada@gmail.com>
2023-11-26 03:45:48 +0100

    Modified Source Code

--- password_checker.py
+++ password_checker.py
@@ -24,9 +24,9 @@ if re.search("[0-9]", user_password):
 
 #Using colors to indicate bad or good complexity score
 if complexity <= 3:
-    print(f"\nYour password complexity score is {Fore.RED}{complexity}{Fore.RESET}/7 {emoji.emojize(':thumbs_down:')}")
+    print(f"\nYour password complexity score is {Style.BRIGHT}{Fore.RED}{complexity}/7{Fore.RESET}{Style.RESET_ALL} {emoji.emojize(':thumbs_down:')}")
 elif complexity > 3:
-     print(f"\nYour password complexity score is {Fore.GREEN}{complexity}{Fore.RESET}/7 {emoji.emojize(':thumbs_up:')}")
+     print(f"\nYour password complexity score is {Style.BRIGHT}{Fore.GREEN}{complexity}/7{Fore.RESET}{Style.RESET_ALL} {emoji.emojize(':thumbs_up:')}")
 
 
 #Testing password security against known common passwords
@@ -38,5 +38,5 @@ print(f"\n---Checking your password {Style.BRIGHT}{user_password}{Style.RESET_AL
 if user_password in track(common_pass, description='[green]Please wait...'):
     print(f"\nFound in common passwords list, can be cracked in less than a min {emoji.emojize(':crying_face:')}; not secure!")
 else:
-    print(f"\nPassword is secure, enough to waste the hackers time a lil! {emoji.emojize(':winking_face:')}")
+    print(f"\nPassword is secure, at least to stall the hacker for some time! {emoji.emojize(':winking_face:')}")
 
